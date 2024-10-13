ba1a71449a5de316aa266549e0d838a1eaeec0e8
Virinas-code <virinas-code@localhost.localdomain>
2024-10-13 13:23:50 +0200

    f2

--- src/player_data.rs
+++ src/player_data.rs
@@ -1,132 +1,55 @@
 use std::error::Error;
 
-pub struct DataIdentificationNumber(u16);
+trait Line {
+    fn get_data_identification_number() -> DataIdentificationNumber;
 
-impl TryFrom<u16> for DataIdentificationNumber {
-    type Error = Box<dyn Error>;
-
-    fn try_from(value: u16) -> Result<Self, Self::Error> {
-        if 0 <= value && value <= 999 {
-            return Ok(Self(value));
-        } else {
-            return Err(Box::from(
-                "A data identification number must be a 3 digit code",
-            ));
-        }
-    }
+    fn get_fields() -> Vec<Box<impl Field>>;
 }
 
-impl Into<u16> for DataIdentificationNumber {
-    fn into(self: &Self) -> u16 {
-        return self.0;
-    }
+trait Field: TryFrom<String> + Into<String> {
+    fn get_size() -> Option<u8>; // None means variable size but can only be at the end of a line
 }
+
+struct DataIdentificationNumber(u16);
 
 impl TryFrom<String> for DataIdentificationNumber {
     type Error = Box<dyn Error>;
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
-        Self::try_from(str::parse::<u16>(value.as_str())?)
+        let parsed: u16 = value.parse::<u16>()?;
+
+        if parsed >= 0 && parsed <= 999 {
+            Ok(Self(parsed))
+        } else {
+            Err(Box::from(
+                "Data identification number must be a number between 0 and 999",
+            ))
+        }
     }
 }
 
 impl Into<String> for DataIdentificationNumber {
-    fn into(self: &Self) -> String {
-        self.0.to_string()
+    fn into(self) -> String {
+        format!("{:0>3}", self.0.to_string())
     }
 }
 
 impl Field for DataIdentificationNumber {
-    const SIZE: u8 = 3;
-}
-
-trait Line {
-    fn get_data_identification_number(self) -> DataIdentificationNumber;
-
-    fn get_fields(self) -> Vec<&'static dyn Field>;
-}
-
-impl Into<String> for Line {
-    fn into(self: Self) -> String {}
-}
-
-trait Field: Into<String> + TryFrom<String> {
-    fn get_size(self) -> u8;
-}
-
-pub struct StartingRankNumber(u16);
-
-impl TryFrom<u16> for StartingRankNumber {
-    type Error = Box<dyn Error>;
-
-    fn try_from(value: u16) -> Result<Self, Self::Error> {
-        if 1 <= value && value <= 9999 {
-            return Ok(Self(value));
-        } else {
-            return Err(Box::from(
-                "A starting rank number must be between 1 and 9999",
-            ));
-        }
+    fn get_size() -> Option<u8> {
+        return Some(3);
     }
 }
 
-impl Into<u16> for StartingRankNumber {
-    fn into(self: Self) -> u16 {
-        return self.0;
+struct PlayerSection {}
+
+impl Line for PlayerSection {
+    fn get_data_identification_number() -> DataIdentificationNumber {
+        return DataIdentificationNumber(3);
     }
-}
 
-enum Sex {
-    Man,
-    Woman,
-}
+    fn get_fields() -> Vec<Box<impl Field>> {
+        let fields: Vec<Box<_>> = Vec::new();
 
-impl Sex {
-    const MAN: char = 'm';
-    const WOMAN: char = 'w';
-}
-
-impl TryFrom<char> for Sex {
-    type Error = Box<dyn Error>;
-
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        match value {
-            Self::MAN => Ok(Self::Man),
-            Self::WOMAN => Ok(Self::Woman),
-            _ => Err(Box::from("Sex must be 'm' or 'w'")),
-        }
+        return fields;
     }
-}
-
-impl Into<char> for Sex {
-    fn into(self: Self) -> char {
-        match self {
-            Self::Man => Self::MAN,
-            Self::Woman => Self::WOMAN,
-        }
-    }
-}
-
-pub enum Title {
-    GM,
-    IM,
-    WGM,
-    FM,
-    WIM,
-    CM,
-    WFM,
-    WCM,
-}
-
-pub struct Name {
-    first_name: String,
-    last_name: String,
-}
-
-pub struct PlayerData {
-    starting_rank_number: StartingRankNumber,
-    sex: Option<Sex>,
-    title: Option<Title>,
-    fide_rating: Option<u16>,
-    fide_federation: Option<String>, // 3 characters country code
 }
