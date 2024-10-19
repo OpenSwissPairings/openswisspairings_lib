15dc67e6e1f15054d4ea25d4ec02d5765ac2619f
Virinas-code <alexcode228@gmail.com>
2024-10-20 01:20:24 +0200

    Strict lint with `deny` isntead of `warn`

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@
 //! // Display a debug view of the Situation:
 //! println!("{:#?}", situation);
 //! ```
-#![warn(
+#![deny(
     missing_docs,
     clippy::missing_docs_in_private_items,
     clippy::all,
